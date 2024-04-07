bebcad8bd193976983702cdc6d0032defd7bd26e
nirakar24 <jenashubham60@gmail.com>
2024-04-07 12:56:14 +0530

    initial

---
+++ movies/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=255)
+    poster = models.ImageField(upload_to='movie_posters/')  
+
+    def __str__(self):
+        return self.title
